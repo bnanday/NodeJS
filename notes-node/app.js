20f1db055c420293704cb2c8829432fb12f4aedf
Bhushan Nanday <bnanday@Bhushans-MacBook-Pro.local>
2017-04-13 02:03:16 -0700

    Adding package.json and notebook

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,25 +1,25 @@
 
+console.log('starting app.js');
+
+//Node Modules
 const fs = require('fs');
 const os = require('os');
 const _ = require('lodash');
-const notes = require('./notes.js');
 
-var user = os.userInfo();
+//Custom modules
+//const notes = require('./notes.js');
 
-var filteredArray = _.uniq([1,1,2,3,4,5,2,3,4]);
+var command = process.argv[2];
+console.log(module);
 
-console.log(filteredArray);
+//This is a comment
 
-// fs.appendFile('greetings.txt',`Hello world ${user.username}`, function(err){
-//     if(err){
-//         console.log(`This is an error ${err}`);
-//     }
-// });
 
-var res = notes.addNote();
-var add = notes.add(1,3);
-console.log(add);
-//console.log(res);
+
+
+
+
+
 
 
 
